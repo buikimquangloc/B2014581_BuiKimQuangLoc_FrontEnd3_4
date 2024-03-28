0b657a36ebb5cb2ea6bfd031ba9e4b17606cef4e
unknown <buikimquangloc@gmail.com>
2024-03-28 07:50:13 +0700

    Tao trang lien he

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,35 @@
 import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
+import NotFound from "@/views/NotFound.vue";
+import ContactEdit from "@/views/ContactEdit.vue";
+import ContactAdd from "@/views/ContactAdd.vue";
 const routes = [
-    {
-        path: "/",
-        name: "contactbook",
-        component: ContactBook,
-    },
-    {
-        path: "/:pathMatch(.*)*",
-        name: "notfound",
-        component: () => import("@/views/NotFound.vue"),
-    },
-    {
-        path: "/contacts/:id",
-        name: "contact.edit",
-        component: () => import("@/views/ContactEdit.vue"),
-        props: true 
-    },
-        
+  {
+    path: "/",
+    name: "contactbook",
+    component: ContactBook,
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: NotFound,
+  },
+  {
+    path: "/contacts/:id",
+    name: "contact.edit",
+    component: ContactEdit,
+    props: true, 
+  },
+  {
+    path: "/contacts/",
+    name: "contact.add",
+    component: ContactAdd,
+  },
 ];
-const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes,
-});
+
+const router = createRouter( {
+  history: createWebHistory( import.meta.env.BASE_URL ),
+  routes,
+} );
 
 export default router;
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -10,4 +10,22 @@ export default (baseURL) => {
         baseURL,
         ...commonConfig,
     });
+
+    instance.interceptors.request.use(
+        ( config ) =>
+        {
+            const token = localStorage.getItem( "token" );
+            if ( token )
+            {
+                config.headers.Authorization = `Bearer ${ token }`;
+            }
+            return config;
+        },
+        ( error ) =>
+        {
+            return Promise.reject( error );
+        }
+    );
+
+    return instance;
 };
